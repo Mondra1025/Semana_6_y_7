5a43e98bce55151ba4c9ee10ddcef42e4bc3d9bf
Alberto Mondragon <ismael20_mondragon@hotmail.com>
2023-10-16 10:49:45 -0500

    Semana 6 y 7

---
+++ agregar_datos_archivo.py
@@ -0,0 +1,7 @@
+archivo=open("archivo_de_prueba.txt","at")
+#\n es para agregar el contenido en una linea al final
+contenido ="\nFuente: RPP"
+
+archivo.write(contenido)
+archivo.close()
+
---
+++ leer_texto.py
@@ -0,0 +1,4 @@
+noticia=open("noticias.txt","rt",encoding
+             ='utf8')
+datos_noticia=noticia.read()
+print(datos_noticia)
